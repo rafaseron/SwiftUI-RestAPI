e7179257ccd10dce9301d7298a4aebb3e1813eca
rafaseron <rafaelseron3011@gmail.com>
2024-05-29 20:58:22 -0400

    Requisicao HTTP a um Endpoint usando URLSession

--- CheffDelivery/ui/Screens/HomeScreen.swift
+++ CheffDelivery/ui/Screens/HomeScreen.swift
@@ -28,7 +28,50 @@ struct HomeScreen: View {
                 
             }
         }.navigationBarBackButtonHidden()
+            .onAppear{
+                getAllStores()
+            }
     }
+    
+    // MARK: - Methods (dentro do escopo struct HomeScreen
+    func getAllStores(){
+        guard let url = URL(string: "https://private-501c8-rafaseron.apiary-mock.com/stores") else { return }
+        
+        //colocado procedimento de requisicao dentro de uma variavel 'task' para que tu possa dar .resume() posteriormente - é OBRIGATÓRIO, se não ele NÃO FAZ a Requisicao
+        //.shared de URLSession garante um Singleton
+        let task = URLSession.shared.dataTask(with: url){ data, response, error in
+            
+            // Verifica se tiver Erro. Caso tenha, nem executa guardar o data e a serializacao JSON
+            if error != nil {
+                guard let errorMessage = error?.localizedDescription else { return }
+                print(errorMessage)
+                
+            } else {
+                guard let storesData = data else { return }
+                
+                // o .jsonObject é uma funcao que pode falhar 'throws -> Any' - entao temos que usar 'try'
+                // o try por sua vez nos obriga a usar a estrutura 'do' 'catch'
+                do {
+                    // '[String:Any]]' -> representa um Dicionário de String,Any (chave-valor)
+                    // [ [String:Any] ] -> representa uma Lista de Dicionários
+                    // Traduzindo em Kotlin -> '[ [String:Any] ]' Representa uma List<Map<String,Any>>
+                    
+                    let json = try JSONSerialization.jsonObject(with: storesData) as? [[String: Any]]
+                    guard let response = json else {return}
+                    print(response)
+                    
+                } catch{
+                    
+                }
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    
 }
 
 #Preview {
