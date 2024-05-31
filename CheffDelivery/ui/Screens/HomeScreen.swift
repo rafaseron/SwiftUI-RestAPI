885a5255c87f62d7f8086abbd4024864e7c450b0
rafaseron <rafaelseron3011@gmail.com>
2024-05-31 14:29:15 -0400

    Chamando uma funcao async e que pode retornar throws. Temos que garantir Escopo Assincrono para executar a funcao e verificar qual sera o retorno antes de executar acoes com o mesmo

--- CheffDelivery/ui/Screens/HomeScreen.swift
+++ CheffDelivery/ui/Screens/HomeScreen.swift
@@ -12,6 +12,7 @@ struct HomeScreen: View {
     // MARK: - Atributos
     
     private let service = StoresService()
+    @State private var listStores: [Loja] = []
     
     // MARK: - View
     
@@ -36,8 +37,33 @@ struct HomeScreen: View {
             }
         }.navigationBarBackButtonHidden()
             .onAppear{
-                service.getAllStores()
+                //O escopo Task tem a mesma funcao que o escopo de Couroutine em Kotlin
+                //Alem de serem rodadas em Scopo de 'Coroutines', também devem ter o await na frente
+                Task{
+                    await searchForStores()
+                }
             }
+    }
+    
+    // MARK: - Métodos da HomeScreen (escopo HomeScreen View aqui)
+    
+    func searchForStores() async {
+        
+        //Como service.getAllStores() pode retornar .sucess ou .failure, precisamos executar com o 'try', já que não sabemos qual o retorno que virá
+        //o 'try', por sua vez, pede o uso da estrutura 'do-catch'
+        //Usamos um 'switch case' para verificar sucess ou failure
+        
+        do{
+            let result = try await service.getAllStores()
+            switch(result){
+            case .success(let stores):
+                self.listStores = stores
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }catch{
+            print(error.localizedDescription)
+        }
     }
     
 }
